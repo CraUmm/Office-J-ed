ae9e10084748689062b55558c36dcbbff684ee5d
CraUmm <therajatmathur@outlook.com>
2015-09-21 01:21:56 +0530

    New class to store tool constants

---
+++ ExcelCraUmm/src/util/CreateConstants.java
@@ -0,0 +1,18 @@
+package util;
+
+public class CreateConstants {
+
+	public static int tool1 = 1; 
+	public static int tool2 = 2;
+	public static int tool3 = 4;
+	public static int tool4 = 8;
+	public static int tool5 = 16;
+	public static int tool6 = 32;
+	public static int tool7 = 64;
+	public static int tool8 = 128;
+	public static int tool9 = 256;
+	public static int tool10 = 512;
+	public static int tool11 = 1024;
+	public static int tool12 = 2048;
+	public static int tool13 = 4096;
+}
